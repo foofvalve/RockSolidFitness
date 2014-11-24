3bb521becff4a10cf68dd50c5ee12d1be7061fde
foofvalve <goneby30@hotmail.com>
2014-11-24 11:33:28 +1000

    added form validation logic

--- app/src/main/java/com/rocksolidfitness/SessionDetails.java
+++ app/src/main/java/com/rocksolidfitness/SessionDetails.java
@@ -1,14 +1,17 @@
 package com.rocksolidfitness;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
@@ -25,10 +28,15 @@ public class SessionDetails extends Activity
     Button mBtnSetDateOfSession;
     Button mBtnCancelSessionEdit;
     Button mBtnSearchForSport;
+    Button mBtnSaveSession;
+    Button mBtnLookupSessDesc;
     AutoCompleteTextView mAutoTvSessDesc;
     AutoCompleteTextView mAutoTvSport;
+    TextView mTvDurationHours;
+    TextView mTvDurationMinutes;
     DateTime mSessionDate;
-    Button mBtnLookupSessDesc;
+    String mErrorMessages;
+
 
     public SessionDetails()
     {
@@ -46,8 +54,12 @@ public class SessionDetails extends Activity
         dataSource.close();
 
         mBtnSetDateOfSession = (Button) findViewById(R.id.btnSetSessDate);
+        mBtnSaveSession = (Button) findViewById(R.id.btnSaveSession);
         mBtnCancelSessionEdit = (Button) findViewById(R.id.btnCancelSessionDetail);
         mBtnLookupSessDesc = (Button) findViewById(R.id.btnSearchSessionDetail);
+
+        mTvDurationHours = (TextView) findViewById(R.id.editTextDurationHours);
+        mTvDurationMinutes = (TextView) findViewById(R.id.editTextDurationMins);
 
         mAutoTvSessDesc = (AutoCompleteTextView) findViewById(R.id.editTextDescription);
         if (autoCompleteListOfSessDesc != null && autoCompleteListOfSports.length > 0)  //attach autocomplete entries
@@ -68,7 +80,89 @@ public class SessionDetails extends Activity
         addCloseButtonListner();
         addLookupSessDescBtnListener();
         addLookupSportBtnListener();
+        addSaveBtnListener();
     }
+
+    void addSaveBtnListener()
+    {
+        mBtnSaveSession.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /*
+                FragmentManager fm = getFragmentManager();
+
+                if (fm.findFragmentById(android.R.id.content) == null)
+                {
+                    SportsListFragment sportListFragment = new SportsListFragment();
+                    FragmentTransaction transaction = fm.beginTransaction();
+
+                    transaction.addToBackStack(null);
+                    transaction.add(android.R.id.content, sportListFragment);
+                    transaction.commit();
+                } */
+
+                if (!isFormValid())
+                    displayErrorMessages();
+                else
+                {
+                    //persist session to db
+
+
+                    Intent returnIntent = new Intent();
+                    setResult(RESULT_CANCELED, returnIntent);
+                    finish();
+                    // --> notify dataset change !!! here.
+                }
+            }
+        });
+    }
+
+    boolean isFormValid()
+    {
+        mErrorMessages = "";
+        if (mAutoTvSport.getText().toString().length() == 0)
+            mErrorMessages = getString(R.string.validation_sport_error) + "\n";
+
+        if (mAutoTvSessDesc.getText().toString().length() == 0)
+            mErrorMessages = mErrorMessages + getString(R.string.validation_description_error) + "\n";
+
+        int durationHoursLength = mTvDurationHours.getText().toString().length();
+        int durationMinutesLength = mTvDurationMinutes.getText().toString().length();
+
+        if (durationHoursLength == 0 && durationMinutesLength == 0)
+        {
+            if (durationHoursLength == 0)
+                mErrorMessages = mErrorMessages + getString(R.string.validation_duration_hours_error) + "\n";
+
+            if (durationMinutesLength == 0)
+                mErrorMessages = mErrorMessages + getString(R.string.validation_duration_minutes_error) + "\n";
+        }
+        return mErrorMessages.equals("") ? true : false;
+    }
+
+    void displayErrorMessages()
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle(getString(R.string.validation_errors_title));
+
+        alertDialogBuilder
+                .setMessage(mErrorMessages)
+                .setCancelable(false)
+                .setNeutralButton(getString(R.string.validation_button_ok), new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
 
     void addLookupSportBtnListener()
     {
